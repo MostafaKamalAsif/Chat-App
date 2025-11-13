b066d19563b8e39e32234ea9ae154ebedcdbae0f
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-11-13 21:20:44 +0600

    add login design

---
+++ src/app/Components/Login.jsx
@@ -0,0 +1,132 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "/src/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "/src/components/ui/card";
+import { Input } from "/src/components/ui/input";
+import { Label } from "/src/components/ui/label";
+import Container from "./Container";
+const Login = () => {
+  const [current, setcurrent] = useState("sign up");
+  return (
+    <>
+      <Container>
+        <Card className="w-full max-w-sm z-10 ms-auto  ">
+          <CardHeader>
+            <CardTitle className='text-xl capitalize'>{current}</CardTitle>
+            <CardDescription>
+              Enter your email below to {current} to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6 ">
+                {current === "sign up" ? (
+                  <>
+                    <div className="grid gap-2">
+                      <Label htmlFor="text">User name</Label>
+                      <Input
+                        id="user name"
+                        type="text"
+                        placeholder="User name"
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <Label htmlFor="password">Password</Label>
+                      </div>
+                      <Input id="password" type="password" required />
+                    </div>
+                    <div className=" flex  text-sm text-gray-600 text-center gap-x-5">
+                      <input type="checkbox" className="mt-1" />
+                      <p> I agree to the terms of Service privacy Policy</p>
+                    </div>
+                  </>
+                ) 
+                
+                : (
+                  <>
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <Label htmlFor="password">Password</Label>
+                        <a
+                          href="#"
+                          className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                        >
+                          Forgot your password?
+                        </a>
+                      </div>
+                      <Input id="password" type="password" required />
+                    </div>
+                  </>
+                )}
+              </div>
+            </form>
+          </CardContent>
+
+
+          <CardFooter className="flex-col gap-2">
+            {current === "sign up" ? (
+              <>
+                <Button type="submit" className="w-full cursor-pointer  font-bold text-[16px] ">
+                  Create an account
+                </Button>
+                <p className="text-sm text-gray-600 text-cente  ">
+                  
+                  Already have an account?
+                  <button className= "pl-1 text-blue-600 cursor-pointer hover:underline " onClick={() => setcurrent("Login")}>
+                    Login
+                  </button>
+                </p>
+              </>
+            ) : (
+              <>
+                <Button type="submit" className="w-full cursor-pointer text-[16px]">
+                  Login
+                </Button>
+                <p className=" text-sm text-gray-600 text-center">
+                  
+                  Don’t have an account?
+                  <button
+                    className="text-blue-600 pl-1 cursor-pointer hover:underline "
+                    onClick={() => setcurrent("sign up")}
+                  >
+                    Create and account
+                  </button>
+                </p>
+              </>
+            )}
+          </CardFooter>
+        </Card>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Login from "./Components/Login";
 
 export default function Home() {
   return (
    <>
+  <div className="bg-[url('/banner.jpg')] bg-cover bg-center min-h-[800px] py-[100px] w-full">
   
+  <Login/>
+  
+  </div>
    </>
   );
 }
